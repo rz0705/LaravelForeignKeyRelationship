9377d4eb0adecf797fc33c2b2600131ba28ee287
Ronit z <ronitz@webmobtech.com>
2023-12-06 17:13:55 +0530

    Change filter by group

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,39 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
+
+<head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        <title>All Members</title>
-
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"> --}}
-    <!-- Select2 CSS -->
+    <title>{{ config('app.name') }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-    </head>
-    <body>
-        @include('partials.topbar')
-        <div class="container-fluid">
-            <div class="row">
-                @include('partials.sidebar')
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Select2 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+</head>
+
+<body>
+    @include('partials.topbar')
+    <div class="container-fluid">
+        <div class="row">
+            @include('partials.sidebar')
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
+                @yield('content')
             </main>
-            </div>
         </div>
-    </body>
-
-     <!-- jQuery -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-     <!-- Select2 -->
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-
-
-     <script>
-        $("#countries_multiple").select2({
-            placeholder: "Select a programming language",
-            allowClear: true
-        });
-      </script>
+    </div>
+</body>
+@stack('scripts')
 </html>
-
--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -5,109 +5,140 @@
         $group_id = isset($_GET['group']) ? $_GET['group'] : '';
         $selected_groups_id = explode(',', $group_id);
     @endphp
-
-    <div class="d-flex">
-        <h2>All Members</h2>
-        <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-            <div class="flex items-left p-xl-1">
+    <div class="min-h-screen flex flex-col">
+        <header class="py-4">
+            <div class="container mx-auto">
+                <h2>All Members</h2>
+        </header>
+        <div class="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg">
+            <div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md items-left p-xl-1">
                 <label for="search" class="text-gray-600 ">Search:</label>
-                <input type="text" id="search" name="search" value="{{ request('search') }}" class="border-black rounded px-2 py-1 focus:outline-none focus:border-blue-500" onchange="performSearch()" autofocus onfocus="moveCursorToEnd()" >
+                <input type="text" id="search" name="search" value="{{ request('search') }}"
+                    class="border-black rounded px-2 py-1 focus:outline-none focus:border-blue-500"
+                    onchange="performSearch()" autofocus onfocus="moveCursorToEnd()">
+
+                <label for="group-select" class="block text-sm font-medium text-gray-600">Filter By Group:</label>
+                <select id="group-select" name="group-select[]" multiple onchange="filterMembers()"
+                    class="border-black rounded px-2 py-1 focus:outline-none focus:border-blue-500 js-example-basic-multiple"
+                    style="width: 100%;">
+                    {{-- <option value="" selected>Select Group</option> --}}
+                    @foreach ($groups as $group)
+                        <option value="{{ $group->group_id }}" @if (in_array($group->group_id, $selected_groups_id)) selected @endif>
+                            {{ $group->name }}
+                        </option>
+                    @endforeach
+                </select>
+
+                <a href="{{ route('addmember') }}">
+                    <button type="button" class="btn btn-outline-secondary btn-sm">Add Member</button>
+                </a>
             </div>
-
-            <label for="group-select" class="block text-sm font-medium text-gray-600">Filter By Group:</label>
-            <select id="group-select" name="group-select[]" multiple onchange="filterMembers()"
-                class="border-black rounded px-2 py-1 focus:outline-none focus:border-blue-500">
-                <option value="" selected>Select Group</option>
-                @foreach ($groups as $group)
-                    <option value="{{ $group->group_id }}" @if (in_array($group->group_id, $selected_groups_id)) selected @endif>
-                        {{ $group->name }}
-                    </option>
-                @endforeach
-            </select>
         </div>
-        <a href="{{ route('addmember') }}">
-            <button type="button" class="btn btn-outline-secondary btn-sm">Add Member</button>
-        </a>
-    </div><br>
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>Member id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Contact</th>
-                <th>Group</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($members as $member)
-                <tr class="member-row" data-group-id="{{ $member->group->id }}">
-                    <td>{{ $member->member_id }}</td>
-                    <td>{{ $member->name }}</td>
-                    <td>{{ $member->email }}</td>
-                    <td>{{ $member->contact }}</td>
-                    <td>{{ $member->group->name }}</td>
-                    <td>
-                        <a href="{{ route('edit', ['id' => $member->member_id]) }}">
-                            <button type="button" class="btn btn-outline-success btn-sm">Edit</button>
-                        </a>&nbsp;
-                        <button type="button" class="btn btn-outline-danger btn-sm"
-                            onclick="confirmDelete('{{ $member->name }}', '{{ route('delete', ['id' => $member->member_id]) }}')">Delete</button>
-                    </td>
+
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Member id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Contact</th>
+                    <th>Group</th>
+                    <th>Action</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $members->links() }}
+            </thead>
+            <tbody>
+                @foreach ($members as $member)
+                    <tr class="member-row" data-group-id="{{ $member->group->id }}">
+                        <td>{{ $member->member_id }}</td>
+                        <td>{{ $member->name }}</td>
+                        <td>{{ $member->email }}</td>
+                        <td>{{ $member->contact }}</td>
+                        <td>{{ $member->group->name }}</td>
+                        <td>
+                            <a href="{{ route('edit', ['id' => $member->member_id]) }}">
+                                <button type="button" class="btn btn-outline-success btn-sm">Edit</button>
+                            </a>&nbsp;
+                            <button type="button" class="btn btn-outline-danger btn-sm"
+                                onclick="confirmDelete('{{ $member->name }}', '{{ route('delete', ['id' => $member->member_id]) }}')">Delete</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $members->links() }}
 
-    <div id="output"></div>
+        <div id="output"></div>
 
-    <script>
-        function performSearch() {
-            var inputValue = document.getElementById('search').value.trim();
-            var selectedGroupIds = Array.from(document.getElementById('group-select').selectedOptions).map(option => option.value);
+        <script>
+            $(document).ready(function() {
+                $('.js-example-basic-multiple').select2({
+                    width: '59%', // Set a custom width, you can adjust this value accordingly
+                    tags: true,
+                    tokenSeparators: [',', ' '],
+                    templateSelection: function(data, container) {
+                        // Use a custom template for selected options to display pills
+                        return $('<span>' + data.text + '</span>');
+                    },
+                    escapeMarkup: function(markup) {
+                        return markup;
+                    }
+                });
+            });
 
-            // Filter out empty values and join the array into a string
-            var groupQueryParam = selectedGroupIds.filter(Boolean).join(',');
-
-            if (inputValue.length > 0) {
-                var url = "{{ route('members') }}" + '?group=' + groupQueryParam + '&search=' + inputValue;
-                window.location.href = url;
-            } else {
-                // If no search input, you may want to handle it differently or redirect to the default URL
-                window.location.href = "{{ route('members') }}";
+            function toggleDropdown() {
+                var dropdown = document.getElementById("group-select");
+                dropdown.classList.toggle("show-dropdown");
             }
-        }
 
-        function moveCursorToEnd() {
-            var input = document.getElementById('search');
-            if (input) {
-                // Move the cursor to the end of the input
-                input.setSelectionRange(input.value.length, input.value.length);
+            function performSearch() {
+                var inputValue = document.getElementById('search').value.trim();
+                var selectedGroupIds = Array.from(document.getElementById('group-select').selectedOptions).map(option => option
+                    .value);
+
+                var groupQueryParam = selectedGroupIds.filter(Boolean).join(',');
+
+                if (inputValue.length > 0) {
+                    var url = "{{ route('members') }}" + '?group=' + groupQueryParam + '&search=' + inputValue;
+                    window.location.href = url;
+                } else {
+                    window.location.href = "{{ route('members') }}";
+                }
             }
-        }
 
-        function filterMembers() {
-            var selectedGroupIds = Array.from(document.getElementById('group-select').selectedOptions).map(option => option.value);
-
-            // Check if there are selected group IDs
-            if (selectedGroupIds.length > 0) {
-                var url = "{{ route('members') }}" + '?group=' + selectedGroupIds.join(',');
-                window.location.href = url;
-            } else {
-                // If no group is selected, you may want to redirect to the default URL or handle it differently
-                window.location.href = "{{ route('members') }}";
+            function moveCursorToEnd() {
+                var input = document.getElementById('search');
+                if (input) {
+                    input.setSelectionRange(input.value.length, input.value.length);
+                }
             }
-        }
 
-        function confirmDelete(memberName, deleteUrl) {
-            if (confirm(`Are you sure you want to delete ${memberName}?`)) {
-                // If the user clicks "OK" in the confirmation dialog, proceed with the deletion.
-                window.location.href = deleteUrl;
-            } else {
-                // If the user clicks "Cancel," do nothing.
+            function filterMembers() {
+                var selectedGroupIds = Array.from(document.getElementById('group-select').selectedOptions).map(option => option
+                    .value);
+
+                if (selectedGroupIds.length > 0) {
+                    var url = "{{ route('members') }}" + '?group=' + selectedGroupIds.join(',');
+                    window.location.href = url;
+                } else {
+                    window.location.href = "{{ route('members') }}";
+                }
             }
-        }
-    </script>
-@endsection
+
+            function confirmDelete(memberName, deleteUrl) {
+                if (confirm(`Are you sure you want to delete ${memberName}?`)) {
+                    window.location.href = deleteUrl;
+                } else {
+                    // If the user clicks "Cancel," do nothing.
+                }
+            }
+        </script>
+    @endsection
+
+    @push('scripts')
+        <script>
+            $("#group-select").select2({
+                placeholder: "Select Group",
+                allowClear: true
+            });
+        </script>
+    @endpush
