d98afa7b72f6ecb7d946799d0723595360308242
Ronit z <ronitz@webmobtech.com>
2023-12-06 13:30:33 +0530

    Remove Suggestions

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="off" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -31,7 +31,7 @@
 
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" name="name" class="form-control" id="name" value="{{ auth()->user()->name }}">
+                    <input type="text" name="name" class="form-control" id="name" value="{{ old('name',auth()->user()->name) }}" autocomplete="off">
                     @error('name')
                         <p style="color:red">{{ $message }}</p>
                     @enderror
@@ -39,7 +39,7 @@
 
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control" id="email" value="{{ auth()->user()->email }}">
+                    <input type="email" name="email" class="form-control" id="email" value="{{ old('email',auth()->user()->email) }}" autocomplete="off">
                     @error('email')
                         <p style="color:red">{{ $message }}</p>
                     @enderror
