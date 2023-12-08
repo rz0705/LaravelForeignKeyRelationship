bba0d1959ac8527e155f0a972973455ec038eb81
Ronit z <ronitz@webmobtech.com>
2023-12-08 15:38:21 +0530

    Send birthday mail notification to user

---
+++ app/Console/Commands/SendBirthdayNotifications.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Notifications\BirthdayNotification;
+
+class SendBirthdayNotifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'birthday:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send birthday notification to user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::whereMonth('birthdate', now()->format('m'))
+                    ->whereDay('birthdate', now()->format('d'))
+                    ->get();
+
+        foreach ($users as $user) {
+            $user->notify(new BirthdayNotification());
+        }
+
+        $this->info('Birthday notifications sent successfully.');
+    }
+}
