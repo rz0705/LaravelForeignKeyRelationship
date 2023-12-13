fa5c7738c8ecfacaee914dba34634cac91f184ca
Ronit z <ronitz@webmobtech.com>
2023-12-13 16:52:33 +0530

    Use prefix for route

--- routes/web.php
+++ routes/web.php
@@ -18,31 +18,35 @@ use Illuminate\Support\Facades\Auth;
 */
 Auth::routes();
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('members', [MemberController::class, 'member'])->name('members');
-    Route::get('member/add', [MemberController::class, 'addmember'])->name('addmember');
-    Route::post('member/add', [MemberController::class, 'insert'])->name('insert');
-    Route::get('member/edit/{id}', [MemberController::class, 'edit'])->name('edit');
-    Route::post('member/edit/{id}', [MemberController::class, 'update'])->name('update');
-    Route::get('member/delete/{id}', [MemberController::class, 'delete'])->name('delete');
-
-    Route::get('groups', [GroupController::class, 'group'])->name('groups');
-    Route::get('group/add', [GroupController::class, 'addgroup'])->name('addgroup');
-    Route::post('group/add', [GroupController::class, 'insert'])->name('insertgroup');
-    Route::get('group/edit/{id}', [GroupController::class, 'edit'])->name('editgroup');
-    Route::post('group/edit/{id}', [GroupController::class, 'update'])->name('updategroup');
-    Route::get('group/delete/{id}', [GroupController::class, 'delete'])->name('deletegroup');
-
-    Route::get('dashboard', [HomeController::class, 'index'])->name('home');
-    
-    Route::get('profile', function () {
-        return view('profile');
-    })->name("profile");
-    Route::get('updateprofile', [ProfileController::class, 'edit'])->name("updateprofile");
-    Route::post('updateprofile', [ProfileController::class, 'update'])->name("updateprofile.post");
-    Route::post('updatepassword', [ProfileController::class, 'updatepassword'])->name("updatepassword.post");
-});
-
 Route::get('/', function () {
     return redirect('/login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', [HomeController::class, 'index'])->name('home');
+
+    Route::prefix('member')->group(function () {
+        Route::get('add', [MemberController::class, 'addmember'])->name('addmember');
+        Route::post('add', [MemberController::class, 'insert'])->name('insert');
+        Route::get('edit/{id}', [MemberController::class, 'edit'])->name('edit');
+        Route::post('edit/{id}', [MemberController::class, 'update'])->name('update');
+        Route::get('delete/{id}', [MemberController::class, 'delete'])->name('delete');
+    });
+    Route::get('members', [MemberController::class, 'member'])->name('members');
+
+    Route::prefix('group')->group(function () {
+        Route::get('add', [GroupController::class, 'addgroup'])->name('addgroup');
+        Route::post('add', [GroupController::class, 'insert'])->name('insertgroup');
+        Route::get('edit/{id}', [GroupController::class, 'edit'])->name('editgroup');
+        Route::post('edit/{id}', [GroupController::class, 'update'])->name('updategroup');
+        Route::get('delete/{id}', [GroupController::class, 'delete'])->name('deletegroup');
+    });
+    Route::get('groups', [GroupController::class, 'group'])->name('groups');
+
+    Route::get('profile', function () {
+        return view('profile');
+    })->name('profile');
+
+    Route::post('updateprofile', [ProfileController::class, 'update'])->name('updateprofile.post');
+    Route::post('updatepassword', [ProfileController::class, 'updatepassword'])->name('updatepassword.post');
 });
